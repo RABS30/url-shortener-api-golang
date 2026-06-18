a90e448c87e30129633428f2698fa4fe98eb4e81
RABS <rizkyabdulbakkahshiddiq@gmail.com>
2026-06-19 01:36:02 +0700

    Buat bussiness logic untuk menangani short url

--- internal/domain/short_urls.go
+++ internal/domain/short_urls.go
@@ -22,3 +22,11 @@ type ShortUrlsRepository interface {
 	FindByUserId(ctx context.Context, userId int64) ([]ShortUrl, error)
 	FindByShortCode(ctx context.Context, shortCode string) (*ShortUrl, error)
 }
+
+type ShortUrlsService interface {
+	CreateShortUrl(ctx context.Context, userId int64, originalUrl string, ExpiredAt time.Time) (*ShortUrl, error)
+	DeleteShortUrl(ctx context.Context, id int64) error
+	GetShortUrlById(ctx context.Context, id int64) (*ShortUrl, error)
+	GetShortUrlsByUserId(ctx context.Context, id int64) ([]ShortUrl, error)
+	GetShortUrlByShortCode(ctx context.Context, shortCode string) (*ShortUrl, error)
+}
---
+++ internal/service/short_urls.go
@@ -0,0 +1,100 @@
+package service
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"net/url"
+	"shorter-url/internal/domain"
+	"time"
+)
+
+type shortUrlsService struct {
+	repo domain.ShortUrlsRepository
+}
+
+func NewShortUrlService(repo domain.ShortUrlsRepository) domain.ShortUrlsService {
+	return &shortUrlsService{
+		repo: repo,
+	}
+}
+
+func (s *shortUrlsService) CreateShortUrl(ctx context.Context, userId int64, originalUrl string, expiredAt time.Time) (*domain.ShortUrl, error) {
+
+	_, err := url.ParseRequestURI(originalUrl)
+
+	if err != nil {
+		return nil, fmt.Errorf("URL format not valid: %w", err)
+	}
+
+	for range 3 {
+		inputData := &domain.ShortUrl{
+			UserId:      userId,
+			ShortCode:   generateShortCode(),
+			OriginalUrl: originalUrl,
+			ExpiredAt:   expiredAt,
+		}
+
+		var result *domain.ShortUrl
+		result, err = s.repo.Create(ctx, inputData)
+		if err == nil {
+			return result, nil
+		}
+
+		if ctx.Err() != nil {
+			return nil, ctx.Err()
+		}
+	}
+
+	return nil, fmt.Errorf("Failed to create short URL. Please recreate. %w", err)
+}
+
+func (s *shortUrlsService) DeleteShortUrl(ctx context.Context, id int64) error {
+	err := s.repo.Delete(ctx, id)
+	if err != nil {
+		return fmt.Errorf("failed to Delete with ID %d. %w", id, err)
+	}
+
+	return nil
+}
+
+func (s *shortUrlsService) GetShortUrlById(ctx context.Context, id int64) (*domain.ShortUrl, error) {
+	result, err := s.repo.FindById(ctx, id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get short url with ID %d. : %w", id, err)
+	}
+
+	return result, nil
+}
+
+func (s *shortUrlsService) GetShortUrlsByUserId(ctx context.Context, userId int64) ([]domain.ShortUrl, error) {
+	rows, err := s.repo.FindByUserId(ctx, userId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get data by user id %d, %w", userId, err)
+	}
+
+	return rows, nil
+}
+
+func (s *shortUrlsService) GetShortUrlByShortCode(ctx context.Context, shortCode string) (*domain.ShortUrl, error) {
+	result, err := s.repo.FindByShortCode(ctx, shortCode)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get data by short code %s, %w ", shortCode, err)
+	}
+
+	return result, nil
+}
+
+func generateShortCode() string {
+	const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	result := make([]byte, 6) 
+	
+	maxLimit := big.NewInt(int64(len(chars)))
+
+	for i := range result {
+		num, _ := rand.Int(rand.Reader, maxLimit)
+		result[i] = chars[num.Int64()]
+	}
+	return string(result)
+}
